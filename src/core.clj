3592d869ad4626a3fa334ed79a13f3a858328cd6
Ivar Refsdal <ivar.refsdal@nsd.no>
2021-02-08 11:59:03 +0100

    clean up

--- src/core.clj
+++ src/core.clj
@@ -25,58 +25,66 @@
         (debug ":PIPE")
         (vreset! pipe-state :PIPE)))))
 
+(defn close!
+  [^Closeable x]
+  (when x
+    (try
+      (.close x)
+      (catch Exception e
+        nil))))
+
+(defn read-stdin [^InputStream in ^OutputStream to-socket]
+  (debug "reading from System/in ...")
+  (try
+    (let [buf (byte-array 1024)
+          run? (atom true)]
+      (while @run?
+        (let [num-bytes (.read in buf)]
+          (if (not= -1 num-bytes)
+            (do
+              (.write to-socket buf 0 num-bytes)
+              (.flush to-socket))
+            (do
+              (debug "System/in closed!")
+              (reset! run? false))))))
+      ;(debug "done reading from System/in"))
+    (catch Exception e
+      (debug (str "error during reading System/in: " (ex-message e))))))
+
+(defn read-socket [^OutputStream out ^InputStream from-socket]
+  (debug "reading from socket ...")
+  (try
+    (let [buf (byte-array 1024)
+          run? (atom true)]
+      (while @run?
+        (let [num-bytes (.read from-socket buf)]
+          (if (not= -1 num-bytes)
+            (do
+              (.write out buf 0 num-bytes)
+              (.flush out))
+            (do
+              (debug "reading from socket closed!")
+              (reset! run? false)))))
+      (debug "done reading from socket"))
+    (catch Exception e
+      (debug (str "error during reading from socket: " (ex-message e))))))
+
 (defn -main [& args]
   (handle-pipe!)
   (debug "starting proxy ...")
   (let [in (BufferedInputStream. System/in)
         out (BufferedOutputStream. System/out)
         sock (socket "127.0.0.1" 7777)
-        to-socket  (-> sock
-                       ^OutputStream (.getOutputStream)
-                       (BufferedOutputStream.))
+        to-socket (-> sock
+                      ^OutputStream (.getOutputStream)
+                      (BufferedOutputStream.))
         from-socket (-> sock
                         ^InputStream (.getInputStream)
-                        (BufferedInputStream.))
-        close! (fn [^Closeable x]
-                 (when x
-                   (try
-                     (.close x)
-                     (catch Exception e
-                       nil))))]
+                        (BufferedInputStream.))]
     (debug "starting proxy ... OK")
 
-    (let [read-stdin (future
-                       (debug "reading from System/in ...")
-                       (try
-                         (let [buf (byte-array 1024)]
-                           (while @running
-                             (let [num-bytes (.read in buf)]
-                               (if (not= -1 num-bytes)
-                                 (do
-                                   (.write to-socket buf 0 num-bytes)
-                                   (.flush to-socket))
-                                 (do
-                                   (debug "System/in closed!")
-                                   (reset! running false)))))
-                           (debug "done reading from System/in"))
-                         (catch Exception e
-                           (debug (str "error during reading System/in: " (ex-message e))))))
-
-          read-sock (future
-                      (debug "reading from socket ...")
-                      (try
-                        (let [buf (byte-array 1024)]
-                          (while @running
-                            (let [num-bytes (.read from-socket buf)]
-                              (if (not= -1 num-bytes)
-                                (do
-                                  (.write out buf 0 num-bytes)
-                                  (.flush out))
-                                (do
-                                  (debug "reading from socket closed!")
-                                  (reset! running false)
-                                  (close! in)))))
-                          (debug "done reading from socket"))
-                        (catch Exception e
-                          (debug (str "error during reading from socket: " (ex-message e))))))]
-      (shutdown-agents))))
+    (let [read-stdin (future (read-stdin in to-socket))
+          read-sock (future (read-socket out from-socket))]
+      @read-stdin
+      (println "done reading...")
+      #_(shutdown-agents))))
