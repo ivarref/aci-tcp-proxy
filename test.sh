705c22becd657572f0d7b934becaf655c98c2240
Ivar Refsdal <ivar.refsdal@nsd.no>
2021-02-08 15:36:35 +0100

    Prepare release

--- test.sh
+++ test.sh
@@ -9,3 +9,9 @@ clojure -M:native-image
 ./proxy < proxy > out.bin
 
 diff proxy out.bin
+
+mkdir -p resources/ivarref/aci-tcp-proxy
+
+cp -fv ./proxy resources/ivarref/aci-tcp-proxy/proxy
+
+clojure -M:jar
