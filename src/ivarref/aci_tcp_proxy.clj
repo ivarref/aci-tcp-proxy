283a937eadfce8401c9d19bd58552680008806b0
Ivar Refsdal <ivar.refsdal@nsd.no>
2021-02-10 11:34:54 +0100

    ...

--- src/ivarref/aci_tcp_proxy.clj
+++ src/ivarref/aci_tcp_proxy.clj
@@ -111,7 +111,7 @@
           (log/info "entering password ...")
           @(s/put! sock password)
           @(s/put! sock (str remote-host "\n" remote-port "\n"))
-          (log/info "got new websocket connection for" remote-host ":" remote-port)
+          (log/info "got new websocket connection for" (str remote-host ":" remote-port))
           sock)))))
 
 (defn decode [str-chunk]
