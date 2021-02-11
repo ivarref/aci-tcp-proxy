54de92e2b036188e3ca435d1854aefce606b9d7e
Ivar Refsdal <ivar.refsdal@nsd.no>
2021-02-11 15:28:12 +0100

    ...

--- src/ivarref/local_test.clj
+++ src/ivarref/local_test.clj
@@ -34,14 +34,14 @@
       (echo-handler s info))
     {:socket-address (InetSocketAddress. "127.0.0.1" 2222)}))
 
-(defn launch-java-file [f stdout-line-cb]
+(defn launch-java-file [f {:keys [consume-stdout]}]
   (let [new-src (str "#!/usr/bin/java --source 11\n\n" (slurp f))]
     (spit "Runner" new-src)
     (check ($ chmod +x Runner))
     (log/info "launching runner ...")
     (let [pb (->
                (ProcessBuilder. ["/home/ire/code/infra/aci-tcp-proxy/Runner"]))
-               ;(.redirectError ProcessBuilder$Redirect/INHERIT))
+          ;(.redirectError ProcessBuilder$Redirect/INHERIT))
           ^Process proc (.start pb)
           _ (log/info "launching runner ... OK")
           in (BufferedWriter. (OutputStreamWriter. (.getOutputStream proc) StandardCharsets/UTF_8))
@@ -53,13 +53,14 @@
         (log/debug "remote stderr exhausted"))
       (future
         (doseq [lin (line-seq stdout)]
-          (stdout-line-cb lin))
+          (consume-stdout lin))
         (log/debug "remote stdout exhausted"))
       {:in in})))
 
 (comment
-  (let [{:keys [in]} (launch-java-file "src/Hello.java"
-                                       (fn [lin] (log/info lin)))]
+  (let [{:keys [in]} (launch-java-file
+                       "src/Hello.java"
+                       {:consume-stdout (fn [lin] (log/info "got stdout:" lin))})]
     (log/info "launch java returned")
     (.write in "Hello From Clojure\n")
     (Thread/sleep 1000)
