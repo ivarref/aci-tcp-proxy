37030808c864c70bd79b57f242d59e18f23a32bd
Ivar Refsdal <ivar.refsdal@nsd.no>
2021-02-17 10:41:25 +0100

    Add ws-utils

--- src/ivarref/local_test.clj
+++ src/ivarref/local_test.clj
@@ -6,6 +6,7 @@
             [babashka.process :refer [$ check]]
             [clojure.string :as str]
             [ivarref.az-utils :as az-utils]
+            [ivarref.ws-utils :as wu]
             [ivarref.ws-server])
   (:import (java.nio.charset StandardCharsets)
            (java.util Base64)))
@@ -13,61 +14,6 @@
 (defn clear []
   (.print System/out "\033[H\033[2J")
   (.flush System/out))
-
-(defn ws-enc [byt]
-  (assert (bytes? byt))
-  (let [sb (StringBuilder.)]
-    (doseq [b (seq byt)]
-      (let [byte-bin-str (-> (format "%8s" (Integer/toBinaryString (bit-and b 0xff)))
-                             (str/replace " " "0")
-                             (str/replace "0" "_")
-                             (str/replace "1" "!"))]
-        (.append sb byte-bin-str)
-        (.append sb "\n")))
-    (.append sb "$\n")
-    (.toString sb)))
-
-(defn ws-map [m]
-  (assert (map? m))
-  (-> (reduce-kv (fn [o k v]
-                   (str o
-                        (if (keyword? k) (name k) (str k))
-                        "="
-                        (str v)
-                        "\n"))
-                 ""
-                 m)
-      (str/trim)
-      (.getBytes StandardCharsets/UTF_8)
-      (ws-enc)))
-
-(comment
-  (ws-enc (.getBytes " !abcæøåðÿ" StandardCharsets/ISO_8859_1)))
-
-(defn mime-reducer [cb so-far chr]
-  (cond
-    ; ignore echo from stdin on server
-    (contains? #{\! \$ \_} chr)
-    so-far
-
-    ; the char # marks end of mime chunk
-    (= chr \#)
-    (let [decoded (.decode (Base64/getMimeDecoder) ^String so-far)]
-      (log/info "consuming" (alength decoded) "bytes...")
-      (cb decoded)
-      "")
-
-    ; build up mime chunk
-    :else
-    (str so-far chr)))
-
-(defn mime-consumer! [ws cb]
-  (->> ws
-       (s/->source)
-       (s/mapcat (fn [x]
-                   (assert (string? x))
-                   (seq x)))
-       (s/reduce (partial mime-reducer cb) "")))
 
 (defn test-round-trip [ws byt]
   (assert (bytes? byt))
@@ -80,15 +26,15 @@
       (fn [& args]
         (deliver p nil)
         (log/debug "websocket client closed")))
-    (mime-consumer! ws (fn [byte-chunk]
-                         (let [new-chunks (swap! chunks conj byte-chunk)]
-                           (when (= (alength byt)
-                                    (reduce + 0 (map alength new-chunks)))
-                             (deliver p (byte-array (mapcat seq new-chunks)))))))
-    @(s/put! ws (ws-map {:host "127.0.0.1" :port "2222" :logPort "12345"}))
+    (wu/mime-consumer! ws (fn [byte-chunk]
+                            (let [new-chunks (swap! chunks conj byte-chunk)]
+                              (when (= (alength byt)
+                                       (reduce + 0 (map alength new-chunks)))
+                                (deliver p (byte-array (mapcat seq new-chunks)))))))
+    @(s/put! ws (wu/ws-map {:host "127.0.0.1" :port "2222" :logPort "12345"}))
     (doseq [chunk (partition-all 4096 (seq byt))]
       (log/info "pushing chunk of" (count chunk) "bytes")
-      @(s/put! ws (ws-enc (byte-array (vec chunk)))))
+      @(s/put! ws (wu/ws-enc (byte-array (vec chunk)))))
     @p
     (s/close! ws)
     (assert (= (seq @p) (seq byt))
---
+++ src/ivarref/ws_utils.clj
@@ -0,0 +1,59 @@
+(ns ivarref.ws-utils
+  (:require [clojure.string :as str]
+            [clojure.tools.logging :as log]
+            [manifold.stream :as s])
+  (:import (java.nio.charset StandardCharsets)
+           (java.util Base64)))
+
+(defn ws-enc [byt]
+  (assert (bytes? byt))
+  (let [sb (StringBuilder.)]
+    (doseq [b (seq byt)]
+      (let [byte-bin-str (-> (format "%8s" (Integer/toBinaryString (bit-and b 0xff)))
+                             (str/replace " " "0")
+                             (str/replace "0" "_")
+                             (str/replace "1" "!"))]
+        (.append sb byte-bin-str)
+        (.append sb "\n")))
+    (.append sb "$\n")
+    (.toString sb)))
+
+(defn ws-map [m]
+  (assert (map? m))
+  (-> (reduce-kv (fn [o k v]
+                   (str o
+                        (if (keyword? k) (name k) (str k))
+                        "="
+                        (str v)
+                        "\n"))
+                 ""
+                 m)
+      (str/trim)
+      (.getBytes StandardCharsets/UTF_8)
+      (ws-enc)))
+
+(defn mime-reducer [cb so-far chr]
+  (cond
+    ; ignore echo from stdin on server
+    (contains? #{\! \$ \_} chr)
+    so-far
+
+    ; the char # marks end of mime chunk
+    (= chr \#)
+    (let [decoded (.decode (Base64/getMimeDecoder) ^String so-far)]
+      (log/info "consuming" (alength decoded) "bytes...")
+      (cb decoded)
+      "")
+
+    ; build up mime chunk
+    :else
+    (str so-far chr)))
+
+(defn mime-consumer! [ws cb]
+  (->> ws
+       (s/->source)
+       (s/mapcat (fn [x]
+                   (assert (string? x))
+                   (seq x)))
+       (s/reduce (partial mime-reducer cb) "")))
+
