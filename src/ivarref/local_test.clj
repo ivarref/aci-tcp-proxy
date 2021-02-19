12b24f97cd5709770f4bb971d4fb7cac130bcef5
Ivar Refsdal <ivar.refsdal@nsd.no>
2021-02-19 13:02:22 +0100

    ...

--- src/ivarref/local_test.clj
+++ src/ivarref/local_test.clj
@@ -9,7 +9,8 @@
             [ivarref.ws-utils :as wu]
             [ivarref.ws-server]
             [clojure.core.async :as async])
-  (:import (java.nio.charset StandardCharsets)))
+  (:import (java.nio.charset StandardCharsets)
+           (java.net InetSocketAddress)))
 
 (Thread/setDefaultUncaughtExceptionHandler
   (reify Thread$UncaughtExceptionHandler
@@ -20,6 +21,62 @@
 (defn clear []
   (.print System/out "\033[H\033[2J")
   (.flush System/out))
+
+(defn az-websocket []
+  (az-utils/get-websocket {:resource-group "rg-stage-we"
+                           :proxy-path     "/app/lib/Proxy"
+                           :container-name "aci-iretest*"}))
+
+(defn local-websocket []
+  @(http/websocket-client "ws://localhost:3333"))
+
+(def get-ws local-websocket)
+
+(defn tunnel-handler [sock]
+  (log/debug "new connection for echo handler")
+  (wu/redir-handler sock
+                    (delay (get-ws))
+                    {:host    "127.0.0.1"
+                     :port    "2222"
+                     :logPort "12345"}))
+
+(defonce
+  tunnel-server
+  (tcp/start-server
+    (fn [s info]
+      (tunnel-handler s))
+    {:socket-address (InetSocketAddress. "127.0.0.1" 4444)}))
+
+(defn round-trip-2 [byt]
+  (assert (bytes? byt))
+  (let [chunks (atom [])
+        p (promise)
+        sock @(tcp/client {:host "127.0.0.1" :port 4444})]
+    (s/consume
+      (fn [byte-chunk]
+        (assert (bytes? byte-chunk))
+        (let [new-chunks (swap! chunks conj byte-chunk)
+              new-length (reduce + 0 (mapv alength new-chunks))
+              percentage (double (/ (* 100 new-length) (alength byt)))]
+          (log/info "received byte chunk of length" (alength byte-chunk)
+                    ","
+                    (format "%.1f%%" percentage) "done! Total so far:" new-length)
+          (when (= (alength byt) new-length)
+            (log/info "delivering...")
+            (deliver p (byte-array (mapcat seq new-chunks))))))
+      sock)
+    (assert (true? @(s/put! sock byt)))
+    @p
+    (assert (= (seq @p) (seq byt))
+            "round trip test failed!")
+    (s/close! sock)
+    (log/info "done!")))
+
+(comment
+  (round-trip-2 (.getBytes
+                  (str/join "\n" (repeat 100 "Hello World !abcæøåðÿ!"))
+                  StandardCharsets/UTF_8)))
+
 
 (defn test-round-trip [ws byt]
   (assert (bytes? byt))
@@ -68,15 +125,6 @@
           bytes-by-ms (double (/ (alength byt) spent-time))]
       (log/info "round trip test OK in" spent-time "ms, " (format "%.1f" bytes-by-ms)
                 "kB/s! \uD83D\uDE3A \uD83D\uDE3B"))))
-
-(defn az-websocket []
-  (az-utils/get-websocket {:resource-group "rg-stage-we"
-                           :proxy-path     "/app/lib/Proxy"
-                           :container-name "aci-iretest*"}))
-
-(defn local-websocket []
-  @(http/websocket-client "ws://localhost:3333"))
-
 
 (comment
   (do
